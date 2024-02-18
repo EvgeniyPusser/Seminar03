09f3024d793b9484e9d7d193b010f2078128c917
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-18 14:03:00 +0300

    HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿string MyMsg = ReadMsg("Введите целое число");
+if(MyMsg == "q")
+{
+    return;
+}
+
+int MyNumber = Convert.ToInt32(MyMsg);
+int SumOfFigures = SumOfFigures(MyNumber);
+Console.Write(SumOfFigures);
+
+string ReadMsg(string msg)
+{
+   Console.WriteLine(msg);
+   return Console.ReadLine;
+}
+
+int SumOfFigures(int number)
+{
+ int sumOf = 0;
+ while(number != 0)
+ {
+   sumOf = sumOf + number % 10;
+   number = sumOf /10;
+
+ }
+ return sumOf;
+}
+
+
+
+
