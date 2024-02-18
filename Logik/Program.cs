a09440be64105f626564ab72aef84ddbc47ef630
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-18 14:01:46 +0300

    Logik

---
+++ Logik/Program.cs
@@ -0,0 +1,48 @@
+﻿bool A = false;
+bool B = false;
+
+bool formula = !(A && B) && !( !A || B);
+LogicForAll(formula);
+
+
+
+
+void LogicForAll(bool formula){
+    
+    
+   // Console.WriteLine($"A\tB\t!(A && B)");
+
+for (bool A = false; ; A = !A)
+{
+    for (bool B = false; ; B = !B)
+    {
+        bool result = formula;
+        Console.WriteLine($"{A} \t{B} \t{result}");
+
+        if (B == true)
+            break;
+    }
+
+    if (A == true)
+        break;
+}
+}
+
+
+
+// Console.WriteLine($"A\tB\t!(A && B)");
+
+// for (bool A = false; ; A = !A)
+// {
+//     for (bool B = false; ; B = !B)
+//     {
+//         bool result = !(A && B);
+//         Console.WriteLine($"{A}\t{B}\t{result}");
+
+//         if (B == true)
+//             break;
+//     }
+
+//     if (A == true)
+//         break;
+// }
