f29b8deb4a6509633f62c4768dc608845eeed915
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-18 14:03:55 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿InputLoop();
+
+
+InputLoop()
+{
+  while(true)
+  {
+   Console.Write("Введите целое число(или q, чтобы вылететь): ");
+   string input = Console.ReadLine();
+
+   if(input=="q")
+      {
+        break;
+      }
+   ProcessInput(input);
+  }
+
+void ProcessInput(string input)
+{
+  if(int.TryParse(input, out int number))
+  {
+    Console.WriteLine($"Вы и вправду ввели число: {number}");
+    int sumOfDigits = CalculateSumOfDigits(number);
+    Console.WriteLine($"Сумма цифр Вашего числа: {sumOfDigits}");
+
+    if(sumOfDigits % 2 == 0)
+    {
+        Console.WriteLine("Сумма цифр чётная. Вылетаем.");
+        Environment.Exit(0);
+    }
+  }
+  else
+  {
+    Console.WriteLine("Неправильный ввод. Введите целое число.");
+  }
+}
+
+int CalculateSumOfDigits(int number)
+{
+    int sum = 0;
+    while(number != 0)
+    {
+      sum += number % 10;
+      number /= 10;
+
+    }
+    return sum;
+}
+
+}
